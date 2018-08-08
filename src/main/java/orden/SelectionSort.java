13872ba7d2e3ab7413b6c2a22eff1c60f184583f
Juan J. Garcia <j2gl.edu@gmail.com>
2018-08-08 17:19:42 -0600

    Fix format

--- src/main/java/orden/SelectionSort.java
+++ src/main/java/orden/SelectionSort.java
@@ -2,45 +2,43 @@ package orden;
 
 import java.util.Scanner;
 
-
 public class SelectionSort {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-    Scanner teclado = new Scanner(System.in);
-    int A[] = new int [20];
-        int menor,indice,n,i,j;
+        Scanner teclado = new Scanner(System.in);
+        int A[] = new int[20];
+        int menor, indice, n, i, j;
         System.out.print("Ingrese dimension del arreglo: ");
-        n=teclado.nextInt();
+        n = teclado.nextInt();
 
-        for(i=1;i<=n;i++){
-            System.out.print("A["+i+"]= ");
-            A[i]=teclado.nextInt();
+        for (i = 1; i <= n; i++) {
+            System.out.print("A[" + i + "]= ");
+            A[i] = teclado.nextInt();
         }
 
-
-        for(i=1 ;i <= n-1; i++){
+        for (i = 1; i <= n - 1; i++) {
             menor = A[i];
             indice = i;
 
-            for(j=i+1; j<=n; j++){
-             if(menor > A[j]){
-              menor = A[j];
-              indice = j;
-             }
-           }
+            for (j = i + 1; j <= n; j++) {
+                if (menor > A[j]) {
+                    menor = A[j];
+                    indice = j;
+                }
+            }
 
             A[indice] = A[i];
-            A[i]=menor;
+            A[i] = menor;
 
         }
 
         System.out.println("EL ARREGLO ORDENADO");
-        for(i=1;i<=n;i++){
-            System.out.print(" "+A[i]);
+        for (i = 1; i <= n; i++) {
+            System.out.print(" " + A[i]);
 
         }
 
-}
+    }
 
 }
