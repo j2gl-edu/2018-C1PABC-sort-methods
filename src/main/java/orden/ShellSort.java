a37c3fde77b5741441abe28219982b4421c79f90
Juan J. Garcia <j2gl.edu@gmail.com>
2018-08-08 19:55:56 -0600

    Add display

--- src/main/java/orden/ShellSort.java
+++ src/main/java/orden/ShellSort.java
@@ -1,46 +1,50 @@
 package orden;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class ShellSort {
 
-    public static int[] shell(int A[]) {
+    public static void main(String[] args) {
+        int n = 20;
+        Random random = new Random();
+        int[] numbers = new int[n];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(100);
+        }
+        
+        System.out.println("Arreglo         : " + displayArray(numbers));
+        shell(numbers);
+        System.out.println("Arreglo ordenado: " + displayArray(numbers));
+
+    }
+
+    private static int[] shell(int A[]) {
         int salto, aux, i;
         boolean cambios;
         for (salto = A.length / 2; salto != 0; salto /= 2) {
             cambios = true;
             while (cambios) { // Mientras se intercambie algún elemento
                 cambios = false;
-                for (i = salto; i < A.length; i++) // se da una pasada
+                for (i = salto; i < A.length; i++) {// se da una pasada
                     if (A[i - salto] > A[i]) { // y si están desordenados
                         aux = A[i]; // se reordenan
                         A[i] = A[i - salto];
                         A[i - salto] = aux;
                         cambios = true; // y se marca como cambio.
                     }
+                }
             }
         }
         return A;
     }
 
-    public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
-        int A[];
-        int menor, indice, n, i, j;
-        System.out.print("Ingrese dimension del arreglo: ");
-        n = teclado.nextInt();
-        A = new int[n];
-        for (i = 0; i < n; i++) {
-            System.out.print("A[" + i + "]= ");
-            A[i] = teclado.nextInt();
+    private static String displayArray(int[] numbers) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < numbers.length - 1; i++) {
+            stringBuilder.append(numbers[i] + ", ");
         }
-
-        A = shell(A);
-        System.out.println("EL ARREGLO ORDENADO");
-        for (i = 0; i < n; i++) {
-            System.out.print(" " + A[i]);
-
-        }
+        stringBuilder.append(numbers[numbers.length - 1]);
+        return stringBuilder.toString();
     }
 
 }
