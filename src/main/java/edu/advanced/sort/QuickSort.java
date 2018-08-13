29c5de375495e538709cbd8504fa7cb1f0a0909d
Juan J. Garcia <j2gl.edu@gmail.com>
2018-08-13 16:47:33 -0600

    Quick format

--- src/main/java/edu/advanced/sort/QuickSort.java
+++ src/main/java/edu/advanced/sort/QuickSort.java
@@ -6,7 +6,6 @@ public class QuickSort {
     private int length;
 
     public void sort(int[] inputArr) {
-
         if (inputArr == null || inputArr.length == 0) {
             return;
         }
@@ -16,18 +15,17 @@ public class QuickSort {
     }
 
     private void quickSort(int lowerIndex, int higherIndex) {
-
         int i = lowerIndex;
         int j = higherIndex;
         // calculate pivot number, I am taking pivot as middle index number
-        int pivot = array[lowerIndex+(higherIndex-lowerIndex)/2];
+        int pivot = array[lowerIndex + (higherIndex - lowerIndex) / 2];
         // Divide into two arrays
         while (i <= j) {
             /**
-             * In each iteration, we will identify a number from left side which
-             * is greater then the pivot value, and also we will identify a number
-             * from right side which is less then the pivot value. Once the search
-             * is done, then we exchange both numbers.
+             * In each iteration, we will identify a number from left side which is greater
+             * then the pivot value, and also we will identify a number from right side
+             * which is less then the pivot value. Once the search is done, then we exchange
+             * both numbers.
              */
             while (array[i] < pivot) {
                 i++;
@@ -37,7 +35,7 @@ public class QuickSort {
             }
             if (i <= j) {
                 exchangeNumbers(i, j);
-                //move index to next position on both sides
+                // move index to next position on both sides
                 i++;
                 j--;
             }
@@ -55,12 +53,11 @@ public class QuickSort {
         array[j] = temp;
     }
 
-    public static void main(String a[]){
-
+    public static void main(String a[]) {
         QuickSort sorter = new QuickSort();
-        int[] input = {24,2,45,20,56,75,2,56,99,53,12};
+        int[] input = { 24, 2, 45, 20, 56, 75, 2, 56, 99, 53, 12 };
         sorter.sort(input);
-        for(int i:input){
+        for (int i : input) {
             System.out.print(i);
             System.out.print(" ");
         }
