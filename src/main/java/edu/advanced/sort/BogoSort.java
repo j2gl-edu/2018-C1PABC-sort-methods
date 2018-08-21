c534e5cde3638e62c6e9cd4988456c1e46a498c3
Juan J. Garcia <j2gl.edu@gmail.com>
2018-08-20 18:48:35 -0600

    Add counter to see how many randoms tries.

--- src/main/java/edu/advanced/sort/BogoSort.java
+++ src/main/java/edu/advanced/sort/BogoSort.java
@@ -5,9 +5,11 @@ import java.util.*;
 public class BogoSort{
 
   private static final Random generator = new Random();
+  private static int counter = 0;
 
   public static void bogoSort(int[] array)  {
     while (!isSorted(array)) {
+      counter++;
       for (int i = 0; i < array.length; i++){
         int randomPosition = generator.nextInt(array.length);
         int temp = array[i];
@@ -28,8 +30,10 @@ public class BogoSort{
 
   public static void main(String[] args) {
     int [] array = {5,3,0,2,4,1,0,5,2,3,1,4};
+    System.out.println("Size: " + array.length);
     System.out.println("Before: " + Arrays.toString(array));
     bogoSort(array);
     System.out.println("After:  " + Arrays.toString(array));
+    System.out.println("Counter:" + counter);
   }
 }
